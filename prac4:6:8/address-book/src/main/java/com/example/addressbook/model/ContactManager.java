f979fe55d8cfe7c303b633dbd8a436dad59a4a30
RachoUni <n11418656@qut.edu.au>
2025-04-30 16:50:02 +1000

    Add files via upload
    Proof of completing pracs 4, 6 and 8

---
+++ prac4:6:8/address-book/src/main/java/com/example/addressbook/model/ContactManager.java
@@ -0,0 +1,31 @@
+package com.example.addressbook.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactManager {
+    private IContactDAO contactDAO;
+    public ContactManager(IContactDAO contactDAO) {
+        this.contactDAO = contactDAO;
+    }
+
+    public List<Contact> searchContacts(String query) {
+        return contactDAO.getAllContacts()
+                .stream()
+                .filter(contact -> isContactMatched(contact, query))
+                .toList();
+    }
+
+    private boolean isContactMatched(Contact contact, String query) {
+        if (query == null || query.isEmpty()) return true;
+        query = query.toLowerCase();
+        String searchString = contact.getFullName()
+                + " " + contact.getEmail()
+                + " " + contact.getPhone();
+        return searchString.toLowerCase().contains(query);
+    }
+
+    public void addContact(Contact contact) {
+        contactDAO.addContact(contact);
+    }
+}
