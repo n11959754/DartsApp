8c42715fcee4d01e45e0ab40e96e5ce74e1ac478
n12034720 <n12034720@qut.edu.au>
2025-04-30 23:58:47 +1000

    Created a navigation bar system to take user to different pages. Not all pages have been created so can't navigate to some pages.

--- src/main/java/com/darts/dartsapp/controller/ToolBarController.java
+++ src/main/java/com/darts/dartsapp/controller/ToolBarController.java
@@ -1,4 +1,49 @@
 package com.darts.dartsapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
 public class ToolBarController {
+
+    // buttons to take user to different pages //
+    @FXML private Button MainScreen;
+    @FXML private Button Settings;
+    @FXML private Button Announcements;
+
+    // takes user to main screen
+    @FXML
+    protected void onMainScreenClick() throws IOException {
+        Stage stage = (Stage) MainScreen.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/darts/dartsapp/MainScreen-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1324, 768);
+        stage.setScene(scene);
+    }
+
+    // takes user to settings
+    @FXML
+    protected void onSettingsClick() throws IOException {
+        Stage stage = (Stage) Settings.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/darts/dartsapp/SettingsPage-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1324, 768);
+        stage.setScene(scene);
+    }
+
+    // takes user to calendar page
+    @FXML
+    protected void onAnnouncementsClick() throws IOException {
+        Stage stage = (Stage) Announcements.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/darts/dartsapp/calendar-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1324, 768);
+        stage.setScene(scene);
+    }
 }
