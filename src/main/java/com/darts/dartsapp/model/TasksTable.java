ee5c41d775f396484379aea8fd73c10233a6acb8
RachoUni <n11418656@qut.edu.au>
2025-05-26 13:34:16 +1000

    adding tasks table to the db

---
+++ src/main/java/com/darts/dartsapp/model/TasksTable.java
@@ -0,0 +1,111 @@
+package com.darts.dartsapp.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TasksTable {
+
+    private Connection connection;
+
+    public TasksTable() {
+        connection = SqlConnect.getInstance();
+        createTable();
+    }
+
+    private void createTable() {
+        try {
+            Statement statement = connection.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS Tasks ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "assignmentsID INTEGER NOT NULL,"
+                    + "details VARCHAR NOT NULL,"
+                    + "duration INTEGER NOT NULL,"
+                    + "FOREIGN KEY (assignmentsID) REFERENCES Assignments(id)"
+                    + ")";
+            statement.execute(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createTask(Tasks task) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO Tasks (assignmentsID, details, duration) VALUES (?, ?, ?)");
+            statement.setInt(1, task.getAssignmentID());
+            statement.setString(2, task.getDetails());
+            statement.setInt(3, task.getDuration());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateTasks(Tasks task) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE Tasks SET assignmentsID = ?, details = ?, duration = ? WHERE id = ?");
+            statement.setInt(1, task.getAssignmentID());
+            statement.setString(2, task.getDetails());
+            statement.setInt(3, task.getDuration());
+            statement.setInt(4, task.getID());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTask(int id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM Tasks WHERE id = ?");
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Tasks getTask(int id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Settings WHERE id = ?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int assignmentID = resultSet.getInt("assignmentID");
+                String details = resultSet.getString("details");
+                int duration = resultSet.getInt("duration");
+                Tasks task = new Tasks(assignmentID, details, duration);
+                return task;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Tasks> getAllTasks(int id) {
+        List<Tasks> tasks = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Settings WHERE id = ?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int assignmentID = resultSet.getInt("assignmentID");
+                String details = resultSet.getString("details");
+                int duration = resultSet.getInt("duration");
+                Tasks task = new Tasks(assignmentID, details, duration);
+                tasks.add(task);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
+}
